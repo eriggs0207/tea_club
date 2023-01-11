da8645937b6637af83bf2781f65db278712ff24b
Erik Riggs <riggs.erik@gmail.com>
2023-01-11 08:47:08 -0700

    feat: create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,109 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+6.times do
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.safe_email,
+    address: {Faker::Address.full_address
+  )
+end
+
+customers = Customer.all
+
+20.times do
+  Tea.create!(
+    title: Faker::Tea.variety,
+    description: Faker::Coffee.notes,
+    temperature: Faker::Number.between(from: 90, to: 210),
+    brew_time: Faker::Number.between(from: 2, to: 30)
+  )
+end
+
+teas = Tea.all
+
+Subscription.create!(
+  customer_id: customers(0),
+  tea_id: teas(0),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(0),
+  tea_id: teas(1),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(1),
+  tea_id: teas(1),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(1),
+  tea_id: teas(2),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(2),
+  tea_id: teas(3),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(2),
+  tea_id: teas(4),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(3),
+  tea_id: teas(5),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(3),
+  tea_id: teas(6),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(4),
+  tea_id: teas(7),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(4),
+  tea_id: teas(8),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(5),
+  tea_id: teas(9),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
+Subscription.create!(
+  customer_id: customers(5),
+  tea_id: teas(0),
+  title: Faker::Hipster.word,
+  status: rand(0..1),
+  frequency: rand(0..2)
+)
--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :subscription, class: Subscription do
 
-    title {Faker::Name.name}
+    title {Faker::Hipster.word}
     price {Faker::Number.between(from: 10.00, to: 250.00)}
     status { rand(0..1) }
     frequency { rand(0..2) }
