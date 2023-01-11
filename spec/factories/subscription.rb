6754fd7c5d5b734bc47878adb209dc020332bfe1
Erik Riggs <riggs.erik@gmail.com>
2023-01-11 15:20:12 -0700

    feat: update subscription action works all tests pass

--- spec/factories/subscription.rb
+++ spec/factories/subscription.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :subscription, class: Subscription do
     title { Faker::Hipster.word }
     price { Faker::Number.between(from: 10.00, to: 250.00) }
-    status { rand(0..1) }
+    status { 0 }
     frequency { rand(0..2) }
     association :customer, factory: :customer
     association :tea, factory: :tea
